6e7295a85b72720cd0ecd8dce46956f8d1de562e
fms-byte <fsh69711@gmail.com>
2024-04-03 14:12:36 +0600

    fixed the issue

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,10 +7,6 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export const metadata: Metadata = {
-    title: 'Invoices',
-};
-
 export default async function Page({
     searchParams,
 }: {
